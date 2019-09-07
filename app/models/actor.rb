fc80cf6f6c86d90e663d37216920359feaecb10b
Christian Polanco <chrispolanco@sbcglobal.net>
2019-09-07 01:35:26 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
   
+  has_many :characters 
+  has_many :shows, through: :characters 
+  
+  def fullname 
+    "#{self.first.name} #{self.last.name}"
+  end 
+  
+  def list_roles
+    characters.each do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end 
+  
 end
